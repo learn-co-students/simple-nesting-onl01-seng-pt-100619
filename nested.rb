15d7d125bac60b655082e8f8c3deee41a7e2abbb
Eric Manning <emanningmusic@yahoo.com>
2019-10-28 20:52:28 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -79,7 +82,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",
+    :languages => ["LISP", "C"]}
+    
+    programmer_hash
     
 end
 
@@ -101,8 +107,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+ 
+ end
+   
+ end
 end
 
 def adding_to_dennis
