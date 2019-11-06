ed2721d039a740f011cdb20f9e2f99447795227b
Ricky dildy <rdildy92@gmail.com>
2019-11-06 17:30:37 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+           hopper == {:known_for => "COBOL", :languages => {"COBOL", "FORTRAN"}}
 end
 
 def alan_kay_is_known_for
@@ -37,6 +36,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+        known_for.collect |:alan_kay|
+        
+        
 end
 
 def dennis_ritchies_language
@@ -55,7 +57,13 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-end
+     dennis_ritchies_language << adding_matz
+           :yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+           }
+    end 
+   
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -101,6 +109,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     alans_new_info[:alan_kay][known_for].to eq("GUI")
      
      
 end
@@ -121,5 +130,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+       :dennis_ritchie{:languages}<< ("Assembly")
 end
